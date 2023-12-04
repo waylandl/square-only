c02c05c935c728836b2b5968fb67acee544949a4
wayland <97370399+waylandl@users.noreply.github.com>
2023-12-03 22:34:01 -0800

    two by two det

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use std::io::{self, Write};
 
+struct TwoBTwo {
+    data: [f64;4],
+}
+fn two_b_two_determinant(data:&[f64;4])
+-> f64 {
+    let a = data[0];
+    let c = data[1];
+    let b = data[2];
+    let d = data[3];
+
+    a*d - b*c
+}
+
+fn remove_r_c(row: usize, col: usize, data: &Vec<f64>) 
+-> Vec<f64>{
+    let size = (data.len() as f64 ).sqrt() as usize; 
+    let col_end = size * col + 1;
+    let col_start = col_end - size;
+    let filtered_data: Vec<f64> = data
+        .iter()
+        .enumerate()
+        .filter(|&(index,&value)|{
+            (index + 1) % size != row || !(col_start..col_end).contains(&(index + 1))
+        })
+        .map(|(_, &value)| value)
+        .collect();
+   
+    filtered_data
+}
+fn determinant(data: &[f64])
+-> f64 {
+    0.0
+}
 fn main() {
     println!("Enter the size of the vectors:");
     
@@ -7,7 +40,7 @@ fn main() {
     io::stdin().read_line(&mut size).expect("Failed to read line");
     let size: usize = size.trim().parse().expect("Invalid size");
     let size_flat = size * size;
-    let mut matrix_flat = vec![0; size_flat];
+    let mut matrix_flat = vec![0.0; size_flat];
 
     println!("Enter {} vectors :", size);
     
@@ -16,7 +49,7 @@ fn main() {
         io::stdout().flush().expect("Failed to flush");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
-        let numbers: Vec<i32> = input
+        let numbers: Vec<f64> = input
             .split_whitespace()
             .map(|s| s.parse().expect("Invalid number"))
             .collect();
@@ -34,11 +67,12 @@ fn main() {
     }
 
 
+    let matrix_flat2 = remove_r_c(2, 2, &matrix_flat);
 
-    for row in 0..size {
-        for col in 0..size {
-            let index = col * size + row;
-            print!("{} ", matrix_flat[index]);
+    for row in 0..(size-1) {
+        for col in 0..(size-1) {
+            let index = col * (size-1) + row;
+            print!("{} ", matrix_flat2[index]);
         }
         println!();
     }
